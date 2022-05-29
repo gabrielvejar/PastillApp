28f24d37f2622a7248669a86a5a18c282c49dbfc
Gabriel Vejar <g.vejarperez@gmail.com>
2022-05-28 21:18:53 -0400

    Navegacion de main a login activity

--- app/src/main/java/com/gaverez/pastillapp/MainActivity.kt
+++ app/src/main/java/com/gaverez/pastillapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.gaverez.pastillapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnToLogin = findViewById<Button>(R.id.activity_main_btn_login)
+        btnToLogin.setOnClickListener{
+            //Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        val btnToRegister = findViewById<Button>(R.id.activity_main_btn_signin)
+        btnToRegister.setOnClickListener{
+            Toast.makeText(this, "Registrar", Toast.LENGTH_SHORT).show()
+        }
     }
 }
