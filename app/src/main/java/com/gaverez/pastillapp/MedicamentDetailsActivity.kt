a1c8613c263130797be4802ecbfb49a2d12c3fac
Gabriel Vejar <g.vejarperez@gmail.com>
2022-05-29 00:35:40 -0400

    details and edit medicament activities

---
+++ app/src/main/java/com/gaverez/pastillapp/MedicamentDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.gaverez.pastillapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MedicamentDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medicament_details)
+    }
+}
---
+++ app/src/main/java/com/gaverez/pastillapp/MedicamentEditActivity.kt
@@ -0,0 +1,11 @@
+package com.gaverez.pastillapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MedicamentEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_medicament_edit)
+    }
+}
